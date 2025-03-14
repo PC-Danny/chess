2339ea6b788b294c36520f666e1931e03fe2e1de
Danny van Steijn <danny_v_steijn@hotmail.com>
2025-03-14 16:02:23 +0100

    Second commit (big commit). Added classes for pieces and cells, and also defined a start_game function that sets up the board.

---
+++ Lib/pieces.py
@@ -0,0 +1,89 @@
+chess_pieces = [
+    'white_king', 
+    'white_queen', 
+    'white_knight1', 'white_knight2', 
+    'white_bishop1', 'white_bishop2',
+    'white_rook1', 'white_rook2', 
+    'white_pawn1','white_pawn2','white_pawn3','white_pawn4','white_pawn5','white_pawn6','white_pawn7','white_pawn8',
+    'black_king', 
+    'black_queen', 
+    'black_knight1', 'black_knight2', 
+    'black_bishop1','black_bishop2',
+    'black_rook1', 'black_rook2', 
+    'black_pawn1','black_pawn2','black_pawn3','black_pawn4','black_pawn5','black_pawn6','black_pawn7','black_pawn8']
+
+
+class Piece: #TODO alle children van Piece moeten een chess icoon hebben in een variabel piece. 
+    def __init__(self, position, colour, piece, name='temp' , status = 'unmoved',):
+        self.piece = piece
+        self.colour = colour
+        self.position = position
+        self.name = name
+        self.status = status
+        pass
+    def __str__(self):
+        return f"'tis a {self.colour} {self.piece}..."
+    
+    def showMoves(self,moveRange = []):
+        moves = []
+        for x in moveRange:
+            for y in moveRange:
+                moves.append([x,y])
+        moves.remove([0,0])
+        return moves
+
+    def move(self):
+        #!
+
+        #After making your move, enable other buttons
+        for c in cells:
+            if c.colour != self.colour:
+                c.state = 'normal'
+            else:
+                c.state = 'disabled'
+            
+    def castle(self):
+        pass
+
+    def die(self):
+        globals()[f"conquered{(self.colour).capitalize()}Pieces"].append(self.name)
+        self.status = 'defeated'    
+
+class King(Piece):
+    def moves(self,moveRange = range(-1,2)):
+        # include castling move
+        if (self.status=='unmoved'):
+            if globals()[f"{self.colour}_rook1"] == 'unmoved':
+                pass 
+        return super().moves(moveRange)
+
+class Queen(Piece):
+    def moves(self, moveRange = range(-7,8)):
+        # return super().moves(moveRange)
+        return [m for m in super().moves(moveRange) if 0 in m].append([m for m in super().moves(moveRange) if not 0 in m])
+    
+class Rook(Piece):
+    def moves(self, moveRange = range(-7,8)):
+        # include castling move
+        return [m for m in super().moves(moveRange) if 0 in m]
+
+class Bishop(Piece):
+    def moves(self, moveRange = range(-7,8)):
+        print([m for m in super().moves(moveRange) if (not 0 in m) and (abs(m[0])==abs(m[1]))])
+        return [m for m in super().moves(moveRange) if (not 0 in m) and (abs(m[0])==abs(m[1]))]
+
+class Knight(Piece):
+    def moves():
+        return [[dx * x, dy * y] for x, y in [[1, 2], [2, 1]] for dx in [-1, 1] for dy in [-1, 1]]
+    
+class Pawn(Piece):
+    def moves(self):
+        if self.status == 'unmoved':
+            x = 0
+            if self.colour == 'black':
+                return [[x,-1],[x,-2]]
+            else:
+                return [[x,1],[x,2]]
+    
+    def promotion(self):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-horizontal_positions = [1,2,3,4,5,6,7,8]
-vertical_positions = ['a','b','c','d','e','f','g','h']
+import tkinter as tk
+from Lib.pieces import Piece, King, Queen, Knight, Rook, Bishop, Pawn, chess_pieces
+
+horizontal_positions = ['a','b','c','d','e','f','g','h']
+vertical_positions = [1,2,3,4,5,6,7,8]
+
+def chessNotation():
+    pass
+    #TODO Deze functie geeft de moves die zijn gemaakt door in een algebraïsche schaak notatie.
 
 def translatePosition(pos):
     return f"{chr(pos[0]+96)}{pos[1]}"
@@ -11,98 +18,64 @@ def checkmate(king):
         possibleMovesOpponent.append(p.moves())
     if king.position in possibleMovesOpponent:
         return 'checkmate' if set(king.moves).issubset(possibleMovesOpponent) else 'check' 
- 
 
-conqueredBlackPieces = []
-conqueredWhitePieces = []
-
-class Cell:
-    def __init__(self,position,status):
+class Cell(tk.Button):
+    # super().bg = 'black'
+    def __init__(self,position,status = None,*args, **kwargs):
+        tk.Button.__init__(self, *args, **kwargs)
         self.position = position
         self.status = status
         pass
-
-class Piece:
-    piece = ''
-    def __init__(self,position, colour, name, status = 'unmoved'):
-        self.colour = colour
-        self.position = position
-        self.name = name
-        self.status = status
-        pass
-    def __str__(self):
-        return f"'tis a {self.colour} {self.piece}..."
     
-    def moves(self,moveRange = []):
-        moves = []
-        for x in moveRange:
-            for y in moveRange:
-                moves.append([x,y])
-        moves.remove([0,0])
-        return moves
-    def castle(self):
-        pass
-
-    def die(self):
-        globals()[f"conquered{(self.colour).capitalize()}Pieces"].append(self.name)
-        self.status = 'defeated'    
-
-class King(Piece):
-    def moves(self,moveRange = range(-1,2)):
-        # include castling move
-        if (self.status=='unmoved'):
-            if globals()[f"{self.colour}_rook1"] == 'unmoved':
-                pass 
-        return super().moves(moveRange)
-
-class Queen(Piece):
-    def moves(self, moveRange = range(-7,8)):
-        # return super().moves(moveRange)
-        return [m for m in super().moves(moveRange) if 0 in m].append([m for m in super().moves(moveRange) if not 0 in m])
-    
-
-class Rook(Piece):
-    def moves(self, moveRange = range(-7,8)):
-        # include castling move
-        return [m for m in super().moves(moveRange) if 0 in m]
-
-class Bishop(Piece):
-    def moves(self, moveRange = range(-7,8)):
-        return [m for m in super().moves(moveRange) if not 0 in m]
-
-class Knight(Piece):
-    def moves():
-        return [[dx * x, dy * y] for x, y in [[1, 2], [2, 1]] for dx in [-1, 1] for dy in [-1, 1]]
-    
-class Pawn(Piece):
-    def moves(self):
-        if self.status == 'unmoved':
-            x = 0
-            if self.colour == 'black':
-                return [[x,-1],[x,-2]]
-            else:
-                return [[x,1],[x,2]]
-    
-    def promotion(self):
-        pass
-
-    
+    def changeText(self):
+        self.text = 'changed'
+        
+    # tk.Button.bind()  
 
 def startGame():
+    conqueredBlackPieces = []
+    conqueredWhitePieces = []
 
+    window = tk.Tk()
+    frame = tk.Frame()
 
     colours = ['white', 'black']
-
+    #Setting up the pieces
     for c in colours:
-        globals()[f"{c}_king"] = King('e8', c)
-        globals()[f"{c}_queen"] = Queen('e8', c)
-        for i in range(2):
-            globals()[f"{c}_{i+1}"] = ''
+        if c == 'black':
+            ypos1 = 8
+            ypos2 = 7
+        else:
+            ypos1 = 1
+            ypos2 = 2
+        
+        globals()[f"{c}_king"] = King([ypos1,5], c, 'King')
+        globals()[f"{c}_queen"] = Queen([ypos1, 4], c, 'Queen')
+        globals()[f"{c}_rook1"] = Rook([ypos1,1], c, 'Rook')
+        globals()[f"{c}_rook2"] = Rook([ypos1,8], c, 'Rook')
+        globals()[f"{c}_knight1"] = Knight([ypos1,2], c, 'Knight')
+        globals()[f"{c}_knight2"] = Knight([ypos1,7], c, 'Knight')
+        globals()[f"{c}_bishop1"] = Bishop([ypos1,3], c, 'Bishop')
+        globals()[f"{c}_bishop2"] = Bishop([ypos1,6], c, 'Bishop')
+
+        for i in range(1,9):
+            globals()[f"{c}_pawn{i}"] = Pawn([ypos2,i], c, 'Pawn')
+
+    #Setting up the chess grid
+    global cells
+    cells = []
+    for x in range(1,9):
+        for y in range(1,9):
+
+            piece = next((globals()[f"{p}"] for p in chess_pieces if globals()[f"{p}"].position == [x,y]), None)
+            print(x,y,piece)
+
+            globals()[f"cell{x}_{y}"] = Cell(position=translatePosition([x,y]),status=piece, text= f'{piece.colour} {piece.piece}' if piece else '', width = 9, height = 3, master=frame)
+            globals()[f"cell{x}_{y}"].grid(column = y, row = x)
+            cells.append(globals()[f"cell{x}_{y}"])
+
+    frame.pack()
+
+    window.mainloop()
 
 startGame()
-
-def test():
-    test = Bishop(1,2)
-    test.moves()
-
-test()
